e4b9750b5d3b509546c2089b5e8181496ee102e1
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2022-11-24 21:21:13 +0300

    add products search get function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 
 const server = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 server.get('/', (req, res) => {
   res.send('Hello World');
@@ -35,6 +35,15 @@ server.get("/products/category/:category", cors(), (req, res) => {
   }
 })
 
+server.get("/products/search/:search", cors(), (req, res) => {
+  const { search } = req.params;
+  const product = products.filter(product => product.title.toLowerCase().includes(search.toLowerCase()) || product.description.toLowerCase().includes(search.toLowerCase()) || product.category.toLowerCase().includes(search.toLowerCase()) || product.id.toString().includes(search));
+  if (product) {
+    res.status(200).json(product);
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+})
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
