3bbefe0cc20f24d1870b2e64f81ec59015c3b12c
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2023-03-21 22:09:24 +0300

    add dirname

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import express from "express";
 import cors from "cors";
 import products from "./router/products.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(cors());
 app.use("/products", products);
 
 const port = process.env.PORT || 5001;
 
-app.use("/public", express.static("public"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.writeHead(200, { "content-type": "text/html; charset=utf-8" });
